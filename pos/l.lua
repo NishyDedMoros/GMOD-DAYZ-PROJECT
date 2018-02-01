2eb094895deb0857e07cf4ca1471c9a6bb821832
NishyDedMoros <36034908+NishyDedMoros@users.noreply.github.com>
2018-02-01 14:18:45 +0300

    Update l.lua

--- pos/l.lua
+++ pos/l.lua
@@ -1,5 +1,10 @@
 BasicLoot["dayz_ghosttown"] = {
-  
+Vector(-5991,12622,793), -- [ХОЛЛ НАЧ]
+Vector(-5804,12621,793),
+Vector(-5803,12725,793),
+Vector(-5307,12620,793),
+Vector(-5287,12728,793),
+Vector(-5625,12898,816), -- [ХОЛЛ КОН]
 }
 
 WeaponLoot["dayz_ghosttown"] = {
