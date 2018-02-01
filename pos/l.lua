4058f9d76aae317887b99d396b3af6d36a07dd87
Nexeonenn <mr_lesser@mail.ru>
2018-02-01 18:07:27 +0700

    Update l.lua

--- pos/l.lua
+++ pos/l.lua
@@ -3,8 +3,6 @@ BasicLoot["dayz_ghosttown"] = {
 }
 
 WeaponLoot["dayz_ghosttown"] = {
-Vector(0,0,0) -- [ВОЕННАЯ БАЗА - НАЧАЛО]
-Vector(0,0,0) -- [ВОЕННАЯ БАЗА - КОНЕЦ]
 }
 
 MedicLoot["dayz_ghosttown"] = {
